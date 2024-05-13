3eabdb3564e15003054637402f0cf8ec917e8c99
4runner99 <julian7005@gmail.com>
2024-05-13 02:47:59 -0700

    new main menu music

--- app/src/main/java/com/gamecodeschool/snakegame/MainMenuActivity.java
+++ app/src/main/java/com/gamecodeschool/snakegame/MainMenuActivity.java
@@ -3,6 +3,9 @@ package com.gamecodeschool.snakegame;
 import com.bumptech.glide.Glide;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -19,17 +22,43 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainMenuActivity extends Activity {
 
-
+    private MediaPlayer mediaPlayer;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.mainmenu);
+
+        mediaPlayer = new MediaPlayer();
+
+
+
+        try {
+            AssetFileDescriptor assetFileDescriptor = getAssets().openFd("mainmenumusic.ogg");
+            mediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor(), assetFileDescriptor.getStartOffset(), assetFileDescriptor.getLength());
+
+
+            // Prepare the MediaPlayer
+            mediaPlayer.prepare();
+
+            // Start playing the music
+            mediaPlayer.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         ImageView backgroundImage = findViewById(R.id.main_menu);
 
@@ -54,6 +83,16 @@ public class MainMenuActivity extends Activity {
         });
 
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Release the MediaPlayer when the activity is destroyed
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 
 
